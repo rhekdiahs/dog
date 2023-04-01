b6dbfc460d7d7ff7d16802a7245b8d21ef3d443e
CHOI_KH <CHOI_KH@CHOI-KH-PC>
2023-04-01 21:17:23 +0900

    start project

---
+++ dog/src/main/java/kr/spring/dogApplication.java
@@ -0,0 +1,13 @@
+package kr.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class dogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(dogApplication.class, args);
+	}
+
+}
---
+++ dog/src/main/java/kr/spring/member/vo/MemberVO.java
@@ -0,0 +1,25 @@
+package kr.spring.member.vo;
+
+public class MemberVO {
+	private int mem_num;
+	
+	private int mem_auth;
+
+	public int getMem_num() {
+		return mem_num;
+	}
+
+	public void setMem_num(int mem_num) {
+		this.mem_num = mem_num;
+	}
+
+	public int getMem_auth() {
+		return mem_auth;
+	}
+
+	public void setMem_auth(int mem_auth) {
+		this.mem_auth = mem_auth;
+	}
+	
+	
+}
