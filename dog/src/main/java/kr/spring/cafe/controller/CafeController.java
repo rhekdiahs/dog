34f85e93e91aeff78c5d83bd55bf5ea5c3a71c5b
hjjin <hjjin@DESKTOP-8FP6VFD>
2023-04-19 10:27:27 +0900

    진혜정 - a 태그 수정

--- dog/src/main/java/kr/spring/cafe/controller/CafeController.java
+++ dog/src/main/java/kr/spring/cafe/controller/CafeController.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -93,6 +94,8 @@ public class CafeController {
 		cafeService.updateCoords(coord_x, coord_y, h_num);
 	}
 	*/
+	
+	//@GetMapping("/cafe/cafeDetail.do")
 	
 }
 
